a7f8f42dd72a396361831da3c3aead56d56733cb
YoussefMoHlemyAlpha <cds.youssefmhamed30402@alexu.edu.eg>
2025-11-18 18:54:51 +0200

    new commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
 import { AuthInterceptor } from "./interceptors/logger.interceptor";
 
 async function bootstrap() {
@@ -10,5 +9,5 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
   console.log(`server is running on port ${process.env.PORT ?? 5000}`);
   
-}
+} 
 bootstrap();
